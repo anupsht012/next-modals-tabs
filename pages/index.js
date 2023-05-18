ddbfaba167bb5e112174ceb62a7c6b3455732a9d
Anup Shrestha <41245410+anupsht012@users.noreply.github.com>
2023-05-18 11:16:25 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function Home() {
    <>
     <TabsEx/>
     <ModalComp/>
+    <div>
+    <iframe src="https://techxillence-my.sharepoint.com/personal/binod_sharma_techxillence_com/_layouts/15/embed.aspx?UniqueId=07a2da8a-c87f-4313-a1e8-0ea5d4344ac9&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="Call with TechTitans 😍-20230518_102511-Meeting Recording.mp4"></iframe>
+    </div>
    </>
   );
 }
